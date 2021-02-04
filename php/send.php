83df374db02842ee13b3bae8a655baf033c82a93
Dominik Wołowicz <dominikwolowicz@gmail.com>
2021-02-04 10:41:30 +0100

    translate mail template to Polish, and fix contact form dropdown values

--- php/send.php
+++ php/send.php
@@ -1,23 +1,22 @@
 <?php if (!$_POST) exit;
 
-$to	= "intern@rdbgroup.pl";
+    $to	= "kontakt@comfortresidence.pl";
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$subject = $_POST['subject'];
-$msg = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
+    $msg = $_POST['message'];
+    $yardage = $_POST['yardage'];
 
+    $subject 	= "Użytkownik $name przysłał Tobie wiadomość";
 
-$subject 	= "Citrus : HTML Template - ( $subject )  You've been contacted by $name";
+    // $content 	= "$name sent you a message from your enquiry form:\r\n\n";
+    $content   .= "Treść wiadomości: $msg \n\nEmail: $email \n\nNumer telefonu: $phone \n\nMetraż: $yardage";
 
-$content 	= "$name sent you a message from your enquiry form:\r\n\n";
-$content   .= "Contact Reason: $msg \n\nEmail: $email \n\nPhone: $phone \n\n";
-
-if(@mail($to, $subject, $content, "From: $email \r\n Reply-To: $email \r\nReturn-Path: $email\r\n")) {
-    echo "<h5 class='success'>Wiadomość Wysłana</h5>";
-    echo "<br/><p class='success'>Dziękujemy <strong>$name</strong>, twoja wiadomość została wysłana i niedługo ktoś skontaktuje się z tobą.</p>";
-}else{
-    echo "<h5 class='failure'>Przepraszamy, prosimy spróbować ponownie.</h5>";
-}
-?>
+    if(@mail($to, $subject, $content, "From: $email \r\n Reply-To: $email \r\nReturn-Path: $email\r\n")) {
+        echo "<h5 class='success'>Wiadomość Wysłana</h5>";
+        echo "<br/><p class='success'>Dziękujemy <strong>$name</strong>, twoja wiadomość została wysłana i niedługo ktoś skontaktuje się z tobą.</p>";
+    }else{
+        echo "<h5 class='failure'>Przepraszamy, prosimy spróbować ponownie.</h5>";
+    }

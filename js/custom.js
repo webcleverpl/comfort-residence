274a0d124aa2b0a6ea2a14ffb6dc9d2ddc4834cc
Dominik Wołowicz <dominikwolowicz@gmail.com>
2021-02-08 12:17:49 +0100

    add transitions based on migowskiewzgorza.pl

--- js/custom.js
+++ js/custom.js
@@ -11,4 +11,43 @@ window.onload = function () {
 cookiesBtn.addEventListener('click', () => {
     localStorage.setItem('cookies', 'true');
     cookiesContainer.style.display = 'none';
+});
+
+
+// slide-in in view effect
+const isElementXPercentInViewport = function (el, percentVisible) {
+    let
+        rect = el.getBoundingClientRect(),
+        windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+    return !(
+        Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-(rect.height / 1)) * 100)) < percentVisible ||
+        Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
+    )
+};
+
+const left = document.querySelectorAll(".slide-in-left");
+const right = document.querySelectorAll(".slide-in-right");
+const fade = document.querySelectorAll(".custom-fade-in");
+
+window.addEventListener("scroll", () => {
+    left.forEach((el) => {
+        if (isElementXPercentInViewport(el, 10)) {
+            el.classList.add("come-in-left");
+        }
+    });
+
+    right.forEach((el) => {
+        if (isElementXPercentInViewport(el, 10)) {
+            el.classList.add("come-in-right");
+        }
+    });
+
+    fade.forEach((el) => {
+        if (isElementXPercentInViewport(el, 10)) {
+            el.classList.add('fade-in-element');
+            el.classList.remove('hidden');
+        }
+    })
 })
+
